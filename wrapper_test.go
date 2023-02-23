4a82031877517a1d2a65a2323fff8fe3816ad7ba
wwhai <cnwwhai@gmail.com>
2023-02-23 22:51:41 +0800

    fix: fix run pty mode

--- wrapper_test.go
+++ wrapper_test.go
@@ -2,34 +2,35 @@ package socatwrapper
 
 import (
 	"context"
+	"log"
 	"testing"
 	"time"
 )
 
 // go test -timeout 30s -run ^TestSocatServer gosocat-wrapper -v -count=1
-func TestSocatServer(t *testing.T) {
+func _SocatServer() {
 	server := NewSocatServer(60000, 60000+100)
 	port, err := server.StartTunnel(context.WithCancel(context.Background()))
 	if err != nil {
-		t.Fatal(err)
+		log.Fatal(err)
 	}
-	t.Log("StartTunnel on port:", port)
+	log.Println("StartTunnel on port:", port)
 	time.Sleep(20 * time.Second)
 	server.StopTunnel(port)
 }
-func TestSocatClient(t *testing.T) {
+func _SocatClient() {
 	client := NewSocatClient("127.0.0.1", 60001, 7681)
 	err := client.StartTunnel(context.WithCancel(context.Background()))
 	if err != nil {
-		t.Fatal(err)
+		log.Fatal(err)
 	}
-	t.Log("Connect to port:", 60001)
+	log.Println("Connect to port:", 60001)
 	time.Sleep(20 * time.Second)
 	client.Stop()
 }
 func TestProxy(t *testing.T) {
-	go TestSocatServer(t)
+	go _SocatServer()
 	time.Sleep(1 * time.Second)
-	go TestSocatClient(t)
+	go _SocatClient()
 	time.Sleep(29 * time.Second)
 }
